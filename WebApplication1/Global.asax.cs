b5b49febc1b1037046b9282f4e4c2214c645108d
SUBHRANSHU SAHU <SUBHRANSHU SAHU@DESKTOP-NQE4C5H>
2025-08-28 17:34:03 +0530

    Add project files.

---
+++ WebApplication1/Global.asax.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace WebApplication1
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            Application["Total_user"] = 0;
+            Application["Number_of_online_user"] = 0;
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            Application.Lock();
+            Application["Total_user"] = (int)Application["Total_user"] + 1;
+
+            Application["Number_of_online_user"] = (int)Application["Number_of_online_user"] + 1;
+            Application.UnLock();
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+            Application.Lock();
+            Application["Number_of_online_user"] = (int)Application["Number_of_online_user"] - 1;
+            Application.UnLock();
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label1.Text = Application["Total_user"].ToString();
+
+            Label2.Text = Application["Number_of_online_user"].ToString();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Session.Abandon();
+        }
+    }
+}
